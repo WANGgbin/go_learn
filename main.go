0c2ceae886532d54d007e07b3e8d171766dd389b
wangguobin <1842192717@qq.com>
2023-03-08 09:54:22 +0800

    add: funcval

--- main.go
+++ main.go
@@ -1,40 +1,14 @@
 package main
 
-import "encoding/json"
-
-func func1(i, j int64) int64 {
-	return i + j
-}
-
-func func2(i, j, k int64) int64 {
-	return i + j + k
-}
-
-type I interface {
-	setInt(i int)
-	getInt() int
-	print() string
-}
-
-type t struct {
-	i int
-}
-
-func (p t) setInt(i int) {
-	p.i = i
-}
-
-func (p t) getInt() int {
-	return p.i
-}
-
-func (p t) print() string {
-	return "xxx"
-}
-
-func getPrint() func(p t) string {
-	return t.print
+func add(a, b int) int {
+	return a + b
 }
 
 func main() {
+	f := func(a, b int) int {
+		return a + b
+	}
+
+	f(1, 2)
+	add(1, 2)
 }
