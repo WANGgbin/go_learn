7df9d1a885c5deafe18ae0f148918bd39fd68bc7
wangguobin.ultra <wangguobin.ultra@bytedance.com>
2023-03-19 22:50:29 +0800

    add: error_handler/doc.go

--- error_handle/doc.go
+++ error_handle/doc.go
@@ -1,3 +1,16 @@
 package error_handle
 
-// 本包用来描述 go 中，如何优雅的处理错误
+/*
+
+本包用来描述 go 中，如何优雅的处理错误
+1. fmt.Errorf("%w")
+2. errors.As(err error, target any) bool
+3. func Is(err, target error) bool
+4. 应该仅仅处理一次错误，不要重复打印错误。一来代码不简洁，而来错误信息多，不好 debug.
+5. 如果想显示忽略错误，请使用 _ 否则，很难分清楚是要忽略错误还是忘记了错误处理
+*/
+
+func
+func main() {
+
+}
