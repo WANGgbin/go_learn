5c1cd4d19d3fcfe975846ea3a4ac24e5016596ee
wangguobin.ultra <wangguobin.ultra@bytedance.com>
2023-03-19 23:06:14 +0800

    add: error_handler/doc.go

--- error_handle/doc.go
+++ error_handle/doc.go
@@ -8,9 +8,9 @@ package error_handle
 3. func Is(err, target error) bool
 4. 应该仅仅处理一次错误，不要重复打印错误。一来代码不简洁，而来错误信息多，不好 debug.
 5. 如果想显示忽略错误，请使用 _ 否则，很难分清楚是要忽略错误还是忘记了错误处理
+6. 参考：https://github.com/xxjwxc/uber_go_guide_cn#%E9%94%99%E8%AF%AF%E7%B1%BB%E5%9E%8B
 */
 
-func
 func main() {
 
 }
